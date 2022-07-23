d9a7f79614887e510571bd8ebcf88cc42b2c0095
andrejzaha <andrejzaha@yahoo.com>
2022-07-23 14:50:05 +0300

    Changed items container to list

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,37 +1,39 @@
 package com.gildedrose;
 
+import java.util.List;
+
 class GildedRose {
     public static final String ITEM_NAME_SULFURAS = "Sulfuras, Hand of Ragnaros";
     public static final String ITEM_NAME_BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     public static final String ITEM_NAME_AGED_BRIE = "Aged Brie";
-    private final Item[] items;
+    private final List<Item> items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
+    public GildedRose(List<Item> items) {
+        this.items = List.copyOf(items);
     }
 
-    public Item[] getItems() {
+    public List<Item> getItems() {
         return items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (ITEM_NAME_SULFURAS.equals(items[i].name)) {
+        for (int i = 0; i < items.size(); i++) {
+            if (ITEM_NAME_SULFURAS.equals(items.get(i).name)) {
                 continue;
             }
 
             updateQualityBeforeSellIn(i);
 
-            items[i].sellIn -= 1;
+            items.get(i).sellIn -= 1;
 
-            if (items[i].sellIn < 0) {
+            if (items.get(i).sellIn < 0) {
                 updateQualityAfterSellDay(i);
             }
         }
     }
 
     private void updateQualityBeforeSellIn(int i) {
-        if (ITEM_NAME_AGED_BRIE.equals(items[i].name) || ITEM_NAME_BACKSTAGE_PASSES.equals(items[i].name)) {
+        if (ITEM_NAME_AGED_BRIE.equals(items.get(i).name) || ITEM_NAME_BACKSTAGE_PASSES.equals(items.get(i).name)) {
             increaseQuality(i);
             return;
         }
@@ -42,24 +44,24 @@ class GildedRose {
     private void increaseQuality(int i) {
         incrementQuality(i);
 
-        if (ITEM_NAME_BACKSTAGE_PASSES.equals(items[i].name)) {
-            if (items[i].sellIn < 11) {
+        if (ITEM_NAME_BACKSTAGE_PASSES.equals(items.get(i).name)) {
+            if (items.get(i).sellIn < 11) {
                 incrementQuality(i);
             }
-            if (items[i].sellIn < 6) {
+            if (items.get(i).sellIn < 6) {
                 incrementQuality(i);
             }
         }
     }
 
     private void updateQualityAfterSellDay(int i) {
-        if (ITEM_NAME_AGED_BRIE.equals(items[i].name)) {
+        if (ITEM_NAME_AGED_BRIE.equals(items.get(i).name)) {
             incrementQuality(i);
             return;
         }
 
-        if (ITEM_NAME_BACKSTAGE_PASSES.equals(items[i].name)) {
-            items[i].quality = 0;
+        if (ITEM_NAME_BACKSTAGE_PASSES.equals(items.get(i).name)) {
+            items.get(i).quality = 0;
             return;
         }
 
@@ -68,14 +70,14 @@ class GildedRose {
     }
 
     private void incrementQuality(int i) {
-        if (items[i].quality < 50) {
-            items[i].quality += 1;
+        if (items.get(i).quality < 50) {
+            items.get(i).quality += 1;
         }
     }
 
     private void decrementQuality(int i) {
-        if (items[i].quality > 0) {
-            items[i].quality -= 1;
+        if (items.get(i).quality > 0) {
+            items.get(i).quality -= 1;
         }
     }
 
