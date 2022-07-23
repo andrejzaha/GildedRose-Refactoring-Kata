1d589efb37b88958b9de217a9f39801e6c68e27a
andrejzaha <andrejzaha@yahoo.com>
2022-07-23 12:40:51 +0300

    Extracted method to update quality for negative sellIn

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -41,22 +41,31 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+                updateQualityForNegativeSellIn(i);
             }
         }
     }
+
+    private void updateQualityForNegativeSellIn(int i) {
+        if (items[i].name.equals("Aged Brie")) {
+            if (items[i].quality < 50) {
+                items[i].quality += 1;
+            }
+            return;
+        }
+
+        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            items[i].quality = 0;
+            return;
+        }
+
+        if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
+
+        if (items[i].quality > 0) {
+            items[i].quality -= 1;
+        }
+    }
+
 }
